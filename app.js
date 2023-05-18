d0db102febd3fc18e44756f2ae79ad381c29315a
zohir222 <bensalemzohir284@gmail.com>
2023-05-18 16:09:37 +0100

    cors dotenv

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 const express = require('express') ;
 const app = express();
-require("dotenv").config();
-const cors = require ('cors');
+
 
 app.use(express.json()) ;
-app.use(cors());
+
 app.use(express.static('public')) ;
 
 const db = require('./models') ;
